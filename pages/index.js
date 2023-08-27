bd81fed0111ef028b5a4c6951b590149661e5b86
jong-jang <jjh0299@gmail.com>
2023-08-27 11:47:56 +0900

    scss 초기 세팅

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import styles from '@/styles/Home.module.scss';
+import styles from './Home.module.scss';
+console.log(styles);
 
 export default function Home() {
 	return (
@@ -10,8 +11,8 @@ export default function Home() {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main className={`${styles.main}`}>
-				<h1>hello</h1>
+			<main className='main'>
+				<h1>Hello</h1>
 			</main>
 		</>
 	);
