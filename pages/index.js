3d6911164e3f9b82c08bc79db802f425f4c11dd1
jong-jang <jjh0299@gmail.com>
2023-09-02 11:46:41 +0900

    이미지 테스트

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
+import Image from 'next/image';
 import styles from './Home.module.scss';
+import pic1 from '@/public/img/pic1.jpg';
+import pic2 from '@/public/img/pic2.jpg';
+import pic3 from '@/public/img/pic3.jpg';
 
 export default function Home() {
 	return (
 		<>
 			<main className={styles.main}>
-				<h1>Hello</h1>
+				<div>
+					<Image src={pic1} alt='pic' quality={10} fill />
+				</div>
+				<div>
+					{/* 기본적으로 이미지 컴포넌트를 lazy-loading적용 priority로 이미지 로딩 우선순위를 지정하면 자동으로 lazy_loading해제 */}
+					<Image src={pic2} alt='pic' quality={10} fill priority />
+				</div>
+				<div>
+					<Image src={pic3} alt='pic' quality={10} fill />
+				</div>
 			</main>
 		</>
 	);
