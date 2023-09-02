8ff157db504e84307868591da3be26adc20a9a39
jong-jang <jjh0299@gmail.com>
2023-09-02 09:36:41 +0900

    갤러리 생성

---
+++ pages/gallery/index.jsx
@@ -0,0 +1,13 @@
+import styles from './Gallery.module.scss';
+
+function Gallery() {
+	return (
+		<>
+			<main className={styles.main}>
+				<h1>Gallery</h1>
+			</main>
+		</>
+	);
+}
+
+export default Gallery;
