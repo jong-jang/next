0c99e7c232387c0e7362020dd185443ec74929ef
jong-jang <jjh0299@gmail.com>
2023-09-02 10:52:25 +0900

    ssr 설정

--- pages/ssr/index.jsx
+++ pages/ssr/index.jsx
@@ -1,9 +1,20 @@
-function Ssr() {
+function Ssr({ now }) {
 	return (
 		<>
-			<main></main>
+			<main>
+				<p>SSR방식 테스트</p>
+				<h2>{now}</h2>
+			</main>
 		</>
 	);
+}
+
+// 서버단에서 실행 새로고침시 계속 바뀜
+export async function getServerSideProps() {
+	console.log('ssr');
+	return {
+		props: { now: performance.now() },
+	};
 }
 
 export default Ssr;
