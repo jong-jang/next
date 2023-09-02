8b2c76194cacd37532907e3270d117015ceab942
jong-jang <jjh0299@gmail.com>
2023-09-02 09:36:49 +0900

    about 생성

---
+++ pages/about/index.jsx
@@ -0,0 +1,13 @@
+import styles from './About.module.scss';
+
+function About() {
+	return (
+		<>
+			<main className={styles.main}>
+				<h1>about</h1>
+			</main>
+		</>
+	);
+}
+
+export default About;
