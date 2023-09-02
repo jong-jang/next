0014c966ec58cc84ea59c2ccafa62e24b805dd54
jong-jang <jjh0299@gmail.com>
2023-09-02 10:52:38 +0900

    isr 설정

--- pages/isr/index.jsx
+++ pages/isr/index.jsx
@@ -1,9 +1,22 @@
-function Isr() {
+function Isr({ now }) {
 	return (
 		<>
-			<main></main>
+			<main>
+				<p>ISR방식 테스트</p>
+				<h2>{now}</h2>
+			</main>
 		</>
 	);
+}
+
+// ssg 방식에서 revalidate props 추가
+// 설정시간에 따라 갱신
+export async function getStaticProps() {
+	console.log('isr');
+	return {
+		props: { now: performance.now() },
+		revalidate: 5,
+	};
 }
 
 export default Isr;
