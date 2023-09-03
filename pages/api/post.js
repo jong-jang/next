69da9d0bbd539b7a79c49d8c2bf8c249afde59a2
jong-jang <jjh0299@gmail.com>
2023-09-03 11:21:59 +0900

    db 접속 성공여부에따라 해당 정보값을 클라이언트로 전달달

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,6 +1,11 @@
 import { connectMongoDB } from '@/lips/MongoConnect';
 
 export default async function handler(req, res) {
-	const DB = await connectMongoDB();
-	console.log(DB);
+	try {
+		const DB = await connectMongoDB();
+		console.log(DB);
+		res.status(200).send('success');
+	} catch (err) {
+		res.status(400).send({ err });
+	}
 }
