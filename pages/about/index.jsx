4bb0f39e8de1b4af68231da7f8039ae4b2978153
jong-jang <jjh0299@gmail.com>
2023-09-09 11:04:35 +0900

    about 페이지에 회원가입 폼 추가

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,9 +1,27 @@
 import styles from './About.module.scss';
 
 function About() {
+	const [Email, setEmail] = useState('');
+	const [Pwd, setPwd] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.prevnetDefault();
+	};
+
 	return (
 		<>
-			<main className={styles.main}></main>
+			<main className={styles.main}>
+				<h2>Join Member</h2>
+
+				<form onSubmit={handleSubmit}>
+					<input type='email' value={Email} onChange={(e) => setEmail(e.target.value)} />
+					<br />
+					<input type='password' value={Pwd} onChange={(e) => setPwd(e.target.value)} />
+					<br />
+
+					<button type='submit'>회원가입</button>
+				</form>
+			</main>
 		</>
 	);
 }
